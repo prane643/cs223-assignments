2bc35a95d9057f9330470296cf1a86f2953409f6
Pranav Rane <prane@haverford.edu>
2023-02-21 17:12:26 -0500

    updates write

--- A05/test_write.c
+++ A05/test_write.c
@@ -12,9 +12,20 @@
 
 int main(int argc, char** argv) {
   int w, h;
-  struct ppm_pixel* pixels = read_ppm("feep-raw.ppm", &w, &h);
-
+  struct ppm_pixel* pixels1 = read_ppm("feep-raw.ppm", &w, &h);
   // test writing the file to test.ppm, reload it, and print the contents
+  // write data
+  write_ppm("test.ppm",pixels1,w,h);
+  struct ppm_pixel* pixels = read_ppm("test.ppm", &w, &h);
+  int i,j,idx;
+  for (i=0;i<h;i++) {
+    for (j=0;j<w;j++) {
+      idx = i*w+j;
+      printf("(%d,%d,%d) ",pixels[idx].red,pixels[idx].green,pixels[idx].blue);
+    }
+    printf("\n");
+  }
+  printf("\n");
   free(pixels);
   return 0;
 }
--- A05/write_ppm.c
+++ A05/write_ppm.c
@@ -10,7 +10,15 @@
 // implement *one* (but do not remove the other one)!
 
 void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
-
+    FILE * infile;
+    infile = fopen(filename,"wb");
+    // test input
+    //printf("\n(%d,%d,%d)\n",pixels[1].red,pixels[1].green,pixels[1].blue);
+    // write header
+    fprintf(infile,"P6 #testing write file 4 4 255");
+    // write in data
+    fwrite(pixels,sizeof(struct ppm_pixel),w*h,infile);
+    fclose(infile);
 }
 
 void write_ppm_2d(const char* filename, struct ppm_pixel** pixels, int w, int h) {
