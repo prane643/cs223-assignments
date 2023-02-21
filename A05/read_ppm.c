488aa310a2e2490e558ceea301fe8136e93152c1
Pranav Rane <prane@haverford.edu>
2023-02-21 15:56:58 -0500

    updated

--- A05/read_ppm.c
+++ A05/read_ppm.c
@@ -1,6 +1,6 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
+ * Author: Pranav Rane
+ * Date: Feb 21
  * Description
  ---------------------------------------------*/
 #include <stdio.h>
@@ -9,10 +9,29 @@
 #include "read_ppm.h"
 
 // Choose *one* to implement (do not remove the other one!)
-
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
-  return NULL;
+  FILE *infile;
+  infile = fopen(filename,"rb");
+  if (infile==NULL) {
+    return NULL;
+  }
+  // skip header until image size is shown
+  char buff[5000];
+  fgets(buff,128,infile);
+  fgets(buff,128,infile);
+  fscanf(infile, " %d %d%*c",h,w);
+  fgets(buff,5000,infile);
+  // start reading image data
+  struct ppm_pixel* p = malloc(sizeof(struct ppm_pixel)*(*w)*(*h));
+  // return NULL if memory error
+  if (p==NULL) {
+    return NULL;
+  }
+  fread(p, sizeof(struct ppm_pixel),(*w)*(*h),infile);
+  free(infile);
+  return p;
 }
+
 
 struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
   return NULL;
--- A05/test_read.c
+++ A05/test_read.c
@@ -1,6 +1,6 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
+ * Author: Pranav Rane
+ * Date: Feb 21
  * Description
  ---------------------------------------------*/
 #include <stdio.h>
@@ -10,8 +10,17 @@
 int main() {
   int w, h;
   struct ppm_pixel* pixels = read_ppm("feep-raw.ppm", &w, &h);
-
-  // todo: print out the grid of pixels
+  // print out rgb triplets
+  printf("Testing file: %s: %d %d\n","feep-raw.ppm",h,w);
+  int i,j,idx;
+  for (i=0;i<w;i++) {
+    for (j=0;j<h;j++) {
+      idx = i*h + j;
+      printf("(%d,%d,%d) ",pixels[idx].red,pixels[idx].green,pixels[idx].blue);
+    }
+    printf("\n");
+  }
+  printf("\n");
   free(pixels);
   return 0;
 }
