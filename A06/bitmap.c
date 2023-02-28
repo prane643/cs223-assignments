7a177b4cb7b54deb78e0e8e695bae3ede8498818
Pranav Rane <prane@haverford.edu>
2023-02-28 17:00:05 -0500

    updated

--- A06/bitmap.c
+++ A06/bitmap.c
@@ -1,15 +1,28 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Pranav Rane
+ * Date: Feb 28
+ * Description: reads in long int as image data and outputs image
  ---------------------------------------------*/
 #include <stdio.h>
 
 int main() {
   unsigned long img;
   scanf(" %lx", &img);
-  printf("Image (unsigned long): %lx\n", img);
-
+  printf("Image (unsigned long): %lx\n\n", img);
+  unsigned long test;
+  int i;
+  for (i=63;i>=0;i--){
+  test = 1<<i;
+  printf("%lx \n",test);
+  if (test&img == 0){
+    // if here the current bit is zero
+    printf(" ");
+  }
+  else {
+    // if here the current bit is one
+    printf("@");
+  }
+  }
   // todo: your code here
   return 0;
 }
