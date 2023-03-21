13cefc795292f2e31889a2fd897ba302c99bead6
Pranav Rane <prane@haverford.edu>
2023-03-20 21:48:26 -0400

    updated

--- A08/warmup.c
+++ A08/warmup.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main() {
   // testing
+  pid_t pid, mypid;
+  mypid = getpid();
+  printf("%d] A0\n",mypid);
+
+  pid = fork();
+
+  if (pid==0) {
+    // child process
+    mypid = getpid();
+    printf("%d] B1\n",mypid);
+  }
+  else {
+    // parent process
+    mypid = getpid();
+    printf("%d] B0\n",mypid);
+
+    pid = fork();
+    if (pid==0) {
+      // child process
+      mypid = getpid();
+      printf("%d] C1\n",mypid);
+    }
+    else {
+      //parent process
+      mypid = getpid();
+      printf("%d] C0\n",mypid);
+    }
+  }
+  mypid = getpid();
+  printf("%d] Bye\n",mypid);
   return 0;
 }
