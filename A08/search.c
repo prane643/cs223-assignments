fc1b9f1c2fdfbdbe398ff27a749de17a86cb630f
Pranav Rane <prane@haverford.edu>
2023-03-27 15:30:34 -0400

    updated

--- A08/search.c
+++ A08/search.c
@@ -1,5 +1,22 @@
+/*----------------------------------------------
+ * Author: Pranav Rane
+ * Date: Mar 27
+ * Description: Using two processes to search for a value in data.bin
+ ---------------------------------------------*/
+
+
 #include <stdio.h>
+# include <stdlib.h>
 
 int main() {
+  //struct point p[3];
+  FILE* fp = fopen("data.bin", "rb");
+  int *numOfInt;
+  fread(numOfInt, sizeof(int),1, fp);
+  
+
+
+
+  fclose(fp);
   return 0;
 }
