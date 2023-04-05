accc6e90b3bcf20dd292aef1369ffa82658c6be5
Pranav Rane <prane@haverford.edu>
2023-04-05 14:13:54 -0400

    updated

--- A09/search.c
+++ A09/search.c
@@ -12,8 +12,8 @@ struct information {
 
 //void *searchForValue(struct information *tInfo) {
 void *searchForValue(void *arg) { 
-  struct information tInfo;
-  tInfo = *((struct information)arg);
+  struct information *tInfo;
+  tInfo = (struct information*)arg;
   // search for value in data.bin
   int targetValue,tNumber,size,j;
   targetValue = tInfo->value;
@@ -24,13 +24,14 @@ void *searchForValue(void *arg) {
   if (tInfo->lastSegmentSize==0) {
     for (j=0;j<size;j++) {
       if (data[tNumber*size+j]==targetValue){
-        //free(data);
-        printf("\nThread %d found %d at index %d",tNumber+1,targetValue,tNumber*size+j);
-        return 0;
+        free(tInfo);
+        printf("\nThread %d found %d at index %d\n",tNumber+1,targetValue,tNumber*size+j);
+        return NULL;
       }
     }
-    //free(data);
-    return 0;
+    printf("Not found!\n");
+    free(tInfo);
+    return NULL;
   }
   else {
     // if here on last segment with different number of elements
@@ -38,27 +39,32 @@ void *searchForValue(void *arg) {
     lastSegSize = tInfo->lastSegmentSize;
     for (j=0;j<lastSegSize;j++) {
       if (data[tNumber*size+j]==targetValue){
-        //free(data);
-        printf("\nThread %d found %d at index %d",tNumber+1,targetValue,tNumber*size+j);
-        return 0;
+        free(tInfo);
+        printf("\nThread %d found %d at index %d\n",tNumber+1,targetValue,tNumber*size+j);
+        return NULL;
       }
     }
-    //free(data);
-    return 0;
+    printf("Not found!\n");
+    free(tInfo);
+    return NULL;
   }
-  //free(data);
-  return 0;
+  free(tInfo);
+  return NULL;
 }
 
-
-
+int main() {
+/*
 int main(int argc, char** argv) {
+  
   if (argc < 2) {
     printf("usage: %s <NumThreads>\n", argv[0]);
     exit(0);
   }
   int N;
   N = atoi(argv[0]);
+  */
+  int N;
+  N = 2;
   FILE* fp = fopen("data.bin", "rb");
   int *numOfInt = malloc(sizeof(int));
   fread(numOfInt, sizeof(int),1, fp);
@@ -81,20 +87,22 @@ int main(int argc, char** argv) {
   //pthread_t threads[N];
   pthread_t *threads;
   threads = (pthread_t *)malloc(sizeof(pthread_t)*N);
-  struct information threadInformation[N];
+  struct information *threadInformation;
+  //threadInformation = malloc(sizeof(struct information)*N);
   for (i=0;i<N;i++) {
-    threadInformation[i].value = input;
-    threadInformation[i].threadNumber = i;
-    threadInformation[i].data = intList;
+    threadInformation = malloc(sizeof(struct information));
+    (*threadInformation).value = input;
+    (*threadInformation).threadNumber = i;
+    (*threadInformation).data = intList;
     if (i!=N-1) {
-      threadInformation[i].segmentSize = segmentSize;
-      threadInformation[i].lastSegmentSize = 0;
-      pthread_create(&threads[i],NULL,searchForValue,&threadInformation[i]);
+      (*threadInformation).segmentSize = segmentSize;
+      (*threadInformation).lastSegmentSize = 0;
+      pthread_create(&threads[i],NULL,searchForValue,(void *) threadInformation);
     }
     else {
-      threadInformation[i].segmentSize = segmentSize;
-      threadInformation[i].lastSegmentSize = segmentSize+remaining;
-      pthread_create(&threads[i],NULL,searchForValue,&threadInformation[i]);
+      (*threadInformation).segmentSize = segmentSize;
+      (*threadInformation).lastSegmentSize = segmentSize+remaining;
+      pthread_create(&threads[i],NULL,searchForValue,(void *) threadInformation);
     }
   }
   // join threads together
@@ -104,6 +112,7 @@ int main(int argc, char** argv) {
   fclose(fp);
   free(numOfInt);
   free(intList);
+  free(threads);
   return 0;
   }
 
