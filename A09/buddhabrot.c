5810738a1b102b47efb8d7db507bd35423037dc9
Pranav Rane <prane@haverford.edu>
2023-04-11 23:49:45 -0400

    updated

--- A09/buddhabrot.c
+++ A09/buddhabrot.c
@@ -1,8 +1,15 @@
+/*----------------------------------------------
+ * Author: Pranav Rane
+ * Date: Apr 11
+ * Description: Compute buddhabrot with counts MT
+ ---------------------------------------------*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <assert.h>
 #include <time.h>
+#include <sys/time.h>
 #include <pthread.h>
 #include "read_ppm.h"
 #include "write_ppm.h"
@@ -20,7 +27,7 @@ struct threadInformation {
   int size;
   int maxIterations;
   int tid;
-  unsigned char *pallete;
+  //unsigned char *pallete;
   int *membership;
   int *count;
   struct ppm_pixel* imageData;
@@ -34,8 +41,9 @@ void *computeImage(void *arg) {
   int maxIterations,istart,ifinish,jstart,jfinish;
   float xmin,xmax,ymin,ymax;
   struct ppm_pixel* image;
-  unsigned char *pallete;
+  //unsigned char *pallete;
   int *membership;
+  int* count;
   // get information from struct
   xmin = tInfo->xmin;
   xmax = tInfo->xmax;
@@ -46,7 +54,7 @@ void *computeImage(void *arg) {
   row1 = tInfo->row1;
   row2 = tInfo->row2;
   tid = tInfo->tid;
-  pallete = tInfo->pallete;
+  count = tInfo->count;
   membership = tInfo->membership;
   image = tInfo->imageData;
   // identify thread
@@ -125,6 +133,8 @@ void *computeImage(void *arg) {
         pixel.blue = 0;
         */
       }
+      // intialize count array to 0
+      count[imgIdx] = 0;
       //image[imgIdx] = pixel;
       if(j==jfinish-1){
         imgIdx=imgIdx+(size/2);
@@ -135,7 +145,6 @@ void *computeImage(void *arg) {
 
   // start stage 2: compute visited counts
   int yrow,xcol;
-  int* count;
   // create mutex
   pthread_mutex_t mutex;
   pthread_mutex_init(&mutex,NULL);
@@ -179,9 +188,9 @@ void *computeImage(void *arg) {
         }
       }
       if(j==jfinish-1){
-        imgIdx=imgIdx+(size/2);
+        imgIdx1=imgIdx1+(size/2);
       }
-      imgIdx++;
+      imgIdx1++;
    }
   }
   // use barrier to wait for all counts to finish
@@ -191,31 +200,28 @@ void *computeImage(void *arg) {
   float gamma = 0.681;
   float factor = 1.0/gamma;
   float value;
+  struct ppm_pixel color;
   for (i=istart;i<ifinish;i++) {
     for (j=jstart;j<jfinish;j++) {
         value = 0;
-        if (count[i*size+j]>0) { //counts at (row,col) are greater than zero
-            value = log(count[i*size+j]) / log(maxCount);
-            value = pow(value, factor)
-            color.red = value * 255
-            color.green = value * 255
-            color.blue = value * 255
-
-          write color to image at location (row,col)
+        if (count[round(i*size+j)]>0) { //counts at (row,col) are greater than zero
+            value = log(count[round(i*size+j)]) / log(maxCount);
+            value = pow(value, factor);
+            color.red = value * 255;
+            color.green = value * 255;
+            color.blue = value * 255;
+            image[round(i*size+j)] = color; //write color to image at location (row,col)
         }
     }
   }
-
-
+  // free struct
   free(tInfo);
   return NULL;
 }
 
 
-
  // intialize global variable for max count
  static float maxCount = 0;
-
 
 
 int main(int argc, char* argv[]) {
@@ -244,9 +250,118 @@ int main(int argc, char* argv[]) {
   printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
 
-  // todo: your code here
-  // compute image
 
- 
+  // create memory to store image
+  struct ppm_pixel *image = malloc(sizeof(struct ppm_pixel)*size*size);
+  // create  memory to store membership array
+  int *membership = malloc(sizeof(int)*size*size);
+  // create memory for count array
+  int *count = malloc(sizeof(int)*size*size);
 
+  // set up timer
+  double timer;
+  struct timeval tstart, tend;
+  gettimeofday(&tstart, NULL);
+
+  // create four threads
+  int N=4;
+  pthread_t *threads;
+  threads = (pthread_t *)malloc(sizeof(pthread_t)*N);
+
+  // run thread 1
+  struct threadInformation *threadInformation;
+  threadInformation = malloc(sizeof(struct threadInformation));
+  (*threadInformation).xmin = xmin;
+  (*threadInformation).xmax = xmax;
+  (*threadInformation).ymin = ymin;
+  (*threadInformation).ymax = ymax;
+  (*threadInformation).col1 = 0;
+  (*threadInformation).col2 = size/2;
+  (*threadInformation).row1 = size/2;
+  (*threadInformation).row2 = size;
+  (*threadInformation).size = size;
+  (*threadInformation).maxIterations = maxIterations;
+  (*threadInformation).tid = 0;
+  (*threadInformation).membership = membership;
+  (*threadInformation).count = count;
+  (*threadInformation).imageData = image;
+  pthread_create(&threads[0],NULL,computeImage,(void *) threadInformation);
+
+  // run thread 2
+  threadInformation = malloc(sizeof(struct threadInformation));
+  (*threadInformation).xmin = xmin;
+  (*threadInformation).xmax = xmax;
+  (*threadInformation).ymin = ymin;
+  (*threadInformation).ymax = ymax;
+  (*threadInformation).col1 = size/2;
+  (*threadInformation).col2 = size;
+  (*threadInformation).row1 = size/2;
+  (*threadInformation).row2 = size;
+  (*threadInformation).size = size;
+  (*threadInformation).maxIterations = maxIterations;
+  (*threadInformation).tid = 1;
+  (*threadInformation).membership = membership;
+  (*threadInformation).count = count;
+  (*threadInformation).imageData = image;
+  pthread_create(&threads[1],NULL,computeImage,(void *) threadInformation);
+
+  // run thread 3
+  threadInformation = malloc(sizeof(struct threadInformation));
+  (*threadInformation).xmin = xmin;
+  (*threadInformation).xmax = xmax;
+  (*threadInformation).ymin = ymin;
+  (*threadInformation).ymax = ymax;
+  (*threadInformation).col1 = 0;
+  (*threadInformation).col2 = size/2;
+  (*threadInformation).row1 = 0;
+  (*threadInformation).row2 = size/2;
+  (*threadInformation).size = size;
+  (*threadInformation).maxIterations = maxIterations;
+  (*threadInformation).tid = 2;
+  (*threadInformation).membership = membership;
+  (*threadInformation).count = count;
+  (*threadInformation).imageData = image;
+  pthread_create(&threads[2],NULL,computeImage,(void *) threadInformation);
+
+  // run thread 4
+  threadInformation = malloc(sizeof(struct threadInformation));
+  (*threadInformation).xmin = xmin;
+  (*threadInformation).xmax = xmax;
+  (*threadInformation).ymin = ymin;
+  (*threadInformation).ymax = ymax;
+  (*threadInformation).col1 = size/2;
+  (*threadInformation).col2 = size;
+  (*threadInformation).row1 = 0;
+  (*threadInformation).row2 = size/2;
+  (*threadInformation).size = size;
+  (*threadInformation).maxIterations = maxIterations;
+  (*threadInformation).tid = 3;
+  (*threadInformation).membership = membership;
+  (*threadInformation).count = count;
+  (*threadInformation).imageData = image;
+  pthread_create(&threads[3],NULL,computeImage,(void *) threadInformation);
+
+  // join threads together
+  int i;
+  for (i=0;i<N;i++) {
+    pthread_join(threads[i],NULL);
+    printf("\nThread %d) finished",i);
+  }
+
+  // calculate computation time
+  gettimeofday(&tend, NULL);
+  timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
+  printf("\nComputed mandelbrot set (%dx%d) in %g seconds\n",size,size,timer);
+
+  // write image
+  char buffer[100];
+  sprintf(buffer,"mandelbrot-%d-%ld.ppm",size,time(0));
+  write_ppm(buffer,image,size,size);
+
+  // free memory
+  free(membership);
+  free(count);
+  free(image);
+  free(threads);
+  return 0;
 }
